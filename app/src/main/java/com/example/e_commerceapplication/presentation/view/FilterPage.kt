6205df1d3059db251506f7ffb6b15f87b45c8f80
Furkan Keskin <80197323+FurkanKeskinn@users.noreply.github.com>
2022-08-25 10:01:41 +0300

    E_Commerce Application add.

--- app/src/main/java/com/example/e_commerceapplication/presentation/view/FilterPage.kt
+++ app/src/main/java/com/example/e_commerceapplication/presentation/view/FilterPage.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.navigation.fragment.findNavController
-import com.example.e_commerceapplication.FilterPageAdapter
 import com.example.e_commerceapplication.R
 import com.example.e_commerceapplication.databinding.FragmentFilterPageBinding
 
