fc7d8ed961dfe09f613870f97cfafe43739bcf0f
Emrah CETINKAYA <emrah.cetinkaya@getir.com>
2022-09-06 23:49:11 +0300

    bug fix

--- app/src/main/java/com/example/e_commerceapplication/presentation/view/home/HomeFragment.kt
+++ app/src/main/java/com/example/e_commerceapplication/presentation/view/home/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -17,8 +18,7 @@ class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
     private lateinit var productAdapter: ProductRecyclerAdapter
-   //private val viewModel: HomeViewModel by viewModels()
-    private val viewModel: HomeViewModel = ViewModelProvider(this)[HomeViewModel::class.java]
+   private val viewModel: HomeViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
